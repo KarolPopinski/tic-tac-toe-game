9374d578ee64f1480df682a2f81dec8e81c79fda
Karol Popinski <k.popinski@gmail.com>
2023-01-31 21:32:40 +0100

    Game Tic-Tac-Toe version 1.0

--- src/main/java/com/game/TicTacToe/GameApp.java
+++ src/main/java/com/game/TicTacToe/GameApp.java
@@ -9,160 +9,186 @@ public class GameApp {
         Scanner in = new Scanner(System.in);
 
         System.out.println("\nHello!!! Welcome to TicTacToe Game\n");
-        System.out.println("Please enter the value of the board size to prepare: \n"+
-                "3: -> 3 x 3 board\n"+
+        System.out.println("Please enter the value of the board size to prepare: \n" +
+                "3: -> 3 x 3 board\n" +
                 "10: -> 10 x 10 board");
 
-        int boardSize = new Scanner(System.in).nextInt();
+        int boardSize = in.nextInt();
+        int winCondition = boardSize == 3 ? 3 : 5;
         char[][] board = new char[boardSize][boardSize];
 
-        // W metodzie main mam dwóch graczy którzy naprzemian wykonują swoje ruchy do momentu zwycięstwa jednego z nich
-        //albo remisu. Nie wiem, jak dodać wariant gry z komputerem w której komputer będzie losował wartość od 0 do 9
-        // dla osi X i od 0 do 9 dla osi Y. Wiem że poruszanie się po tablicy nie odbywa się po osiach, ale tak jest mi
-        // lepiej sobie wyobrazić. Próbowałem różnych wariantów, ale jedyny który mi wychodzi dotyczy gry na planszy 3x3
-        //gdzie mam zmapowane klawisze 0-9 zapisane wszystki możliwości - wersja z PC w [rzesłanych plikach TXT.
-        //próba rozbudowy tablicy skończyła się fiaskiem, dlatego powróciłem do responsywnej tablicy ale tu nie wiem
-        //jak mam wsadzić computer zamiast Player2?????????? HELP!
+        int gameMode;
+        boolean cpuMode = false;
 
-        System.out.println("Player 1, what is Your name?");
-        String player1 = in.nextLine();
-        System.out.println();
-        System.out.println("Player 2, what is Your name?");
-        String player2 = in.nextLine();
-        System.out.println();
+        String player2;
 
-        boolean isPLayer1 = true;
-        boolean gameEnd = false;
-        while (!gameEnd) {
+        while (true) {
+            System.out.println("MENU\n" +
+                    "select option\n" +
+                    "0 - new game: player v player\n" +
+                    "1 - new game: player v computer\n" +
+                    "2 - quit game");
 
-            printGameBoard(board);
-
-            char symbol = ' ';
-            if (isPLayer1) {
-                symbol = 'X';
-            } else {
-                symbol = 'O';
+            do {
+                gameMode = in.nextInt();
+            } while (gameMode < 0 || gameMode > 3);
+            if (gameMode == 2) {
+                System.out.println("you are leaving the game! Good Bye!");
+                System.exit(0);
             }
 
-            if (isPLayer1) {
-                System.out.println("It is " + player1 + " turn (X):");
-            } else {
-                System.out.println("It is " + player2 + " turn (O):");
+            if (gameMode == 0) {
+                cpuMode = false;
+            } else if (gameMode == 1) {
+                cpuMode = true;
             }
 
+            boolean isPLayer1 = true;
+            boolean gameEnd = false;
+            while (!gameEnd) {
 
-            int row = 0;
-            int column = 0;
+                printGameBoard(board);
 
-            while (true) {
-                System.out.println("please put row number");
-                row = new Scanner(System.in).nextInt();
-                System.out.println("please put column number");
-                column = new Scanner(System.in).nextInt();
-
-                if (row < 0 || column < 0 || row >= boardSize || column >= boardSize) {
-                    System.out.println("Your row and column are out of range!");
-                } else if (board[row][column] != 0) {
-                    System.out.println("Sorry, but this place is occupied.");
+                char symbol = ' ';
+                if (isPLayer1) {
+                    symbol = 'X';
                 } else {
-                    break;
+                    symbol = 'O';
                 }
-            }
-            board[row][column] = symbol;
 
-            if (hasWon(board) == 'X') {
-                System.out.println(player1 + " has won!!!!");
-                gameEnd = true;
-            } else if (hasWon(board) == 'O') {
-                System.out.println(player2 + " has won!!!!");
-                gameEnd = true;
-            } else {
-                if (isTie(board)) {
-                    System.out.println("It is Tie!");
+                if (isPLayer1) {
+                    System.out.println("It is turn (X):");
+                } else {
+                    System.out.println("It is turn (O):");
+                }
+
+
+                int row = 0;
+                int column = 0;
+                boolean keepLooking = true;
+
+                while (keepLooking) {
+
+                    if (!isPLayer1 && cpuMode) {
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board[i].length; j++) {
+                                if (board[i][j] == 0) {
+                                    row = i;
+                                    column = j;
+                                    keepLooking = false;
+                                    break;
+                                }
+                            }
+                            if (!keepLooking) {
+                                break;
+                            }
+                        }
+
+                    } else {
+                        System.out.println("please put row number");
+                        row = new Scanner(System.in).nextInt();
+                        System.out.println("please put column number");
+                        column = new Scanner(System.in).nextInt();
+
+                        if (row < 0 || column < 0 || row >= boardSize || column >= boardSize) {
+                            System.out.println("Your row and column are out of range!");
+                        } else if (board[row][column] != 0) {
+                            System.out.println("Sorry, but this place is occupied.");
+                        } else {
+                            keepLooking = false;
+                        }
+                    }
+
+                }
+
+                board[row][column] = symbol;
+
+                char result = hasWon(board, winCondition);
+                if (result == 'X') {
+                    System.out.println("Player with 'X' has won!!!!");
+                    gameEnd = true;
+                } else if (result == 'O') {
+                    System.out.println("Player with 'O' has won!!!!");
                     gameEnd = true;
                 } else {
-                    isPLayer1 = !isPLayer1;
+                    if (isTie(board)) {
+                        System.out.println("It is Tie!");
+                        gameEnd = true;
+                    } else {
+                        isPLayer1 = !isPLayer1;
+                    }
                 }
             }
+            printGameBoard(board);
+            System.out.println();
+            System.out.println("###GAME OVER###");
         }
-        printGameBoard(board);
-        System.out.println();
-        System.out.println("###GAME OVER###");
     }
 
-    //nie wiem jak przerobić?albo napisać II metodę do sprawdzania warunku zwycięswtwa. Obecna metoda sprawdza warunek
-    //dostosujwac sie do wielkosci tablicy. Jezeli gram na tablic 3x3 super -> sprawdzi wiersze, kolumny i przekatne
-    //super ale jezeli tablica jest 10x10 to jedzie na calej dlugosci. Przez co np X musza byc w calym wierszu albo w calej
-    //kolumnie cyli 10 a warunek zaliczenia to 5 przy tablicy 10x10 - az mnie korcilo zeby podzilic to po prostu przez 2
-    //ale to chyba nie ten kierunek.......ogolnie zalamka :-(
-    //HELP!
-    public static char hasWon(char[][] board) {
+    public static char hasWon(char[][] board, int winCondition) {
 
-        for (int i = 0; i < board.length; i++) {
-            boolean inLine = true;
+        for (int i=0; i < board.length; i++) {
             char value1 = board[i][0];
-            if (value1 == 0) {
-                inLine = false;
-            } else {
-                for (int j = 1; j < board.length; j++) {
+            if (value1 != 0){
+                int count = 1;
+                for (int j=1; j < board.length; j++) {
                     if (board[i][j] != value1) {
-                        inLine = false;
                         break;
+                    }else{
+                        count++;
+                        if(count == winCondition){
+                            return value1;
+                        }
                     }
                 }
             }
-            if (inLine) {
-                return value1;
-            }
         }
 
-        for (int j = 0; j < board.length; j++) {
-            boolean inColumn = true;
+        for (int j=0; j < board.length; j++) {
             char value2 = board[0][j];
-            if (value2 == 0) {
-                inColumn = false;
-            } else {
+            if (value2 != 0) {
+                int count = 1;
                 for (int i = 1; i < board.length; i++) {
                     if (board[i][j] != value2) {
-                        inColumn = false;
                         break;
+                    } else {
+                        count++;
+                        if (count == winCondition) {
+                            return value2;
+                        }
                     }
                 }
             }
-            if (inColumn) {
-                return value2;
-            }
-        }
-        boolean checkDiagonal1 = true;
-        char value3 = board[0][0];
-        if (value3 == 0) {
-            checkDiagonal1 = false;
-        } else {
-            for (int i = 1; i < board.length; i++) {
-                if (board[i][i] != value3) {
-                    checkDiagonal1 = false;
-                    break;
-                }
-            }
-        }
-        if (checkDiagonal1) {
-            return value3;
         }
 
-        boolean checkDiagonal2 = true;
-        char value4 = board[0][board[0].length - 1];
-        if (value4 == 0) {
-            checkDiagonal2 = false;
-        } else {
-            for (int i = 1; i < board.length; i++) {
-                if (board[i][board[0].length - 1 - i] != value4) {
-                    checkDiagonal2 = false;
+        char value3 = board[0][0];
+        if (value3 != 0) {
+           int count = 1;
+           for (int i = 1; i < board.length; i++) {
+                if (board[i][i] != value3) {
                     break;
+                }else{
+                    count++;
+                    if (count == winCondition){
+                        return value3;
+                    }
                 }
             }
         }
-        if (checkDiagonal2) {
-            return value4;
+
+        char value4 = board[0][board[0].length - 1];
+        if (value4 != 0) {
+            for (int i = 1; i < board.length; i++) {
+                int count = 1;
+                if (board[i][board[0].length - 1 - i] != value4) {
+                    break;
+                }else{
+                    count++;
+                    if (count == winCondition) {
+                        return value4;
+                    }
+                }
+            }
         }
         return 0;
     }
